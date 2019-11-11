376915d0f5db96388f5b43a9b65dc8e480a4da80
Kirill Trofimov <kirilltrofimov@inbox.lv>
2019-11-11 23:35:30 +0200

    Fixed tabulation

--- SegDisp/display.cpp
+++ SegDisp/display.cpp
@@ -46,7 +46,7 @@ void DisplayClass::asyncshow(char d1, char d2, char d3, bool dot1, bool dot2, bo
 }
 
 void DisplayClass::show(char d1, char d2, char d3, bool dot1, bool dot2, bool dot3) {
-  unsigned char sym[3] = {
+	unsigned char sym[3] = {
 		(dot1 ? symbol[d1] | 1 : symbol[d1]),
 		(dot2 ? symbol[d2] | 1 : symbol[d2]),
 		(dot3 ? symbol[d3] | 1 : symbol[d3]) };
@@ -58,7 +58,7 @@ void DisplayClass::show(char d1, char d2, char d3, bool dot1, bool dot2, bool do
 		digitalWrite(10 + disp, LOW);
 		delay(5);
 		digitalWrite(10 + disp, HIGH);
-  }
+	}
 }
 
 
