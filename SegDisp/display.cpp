d76c9d60225f1ab57a12e157e21a8a6d57cb20cf
Kirill Trofimov <kirilltrofimov@inbox.lv>
2019-11-15 00:05:15 +0200

    Fixed possible bug

--- SegDisp/display.cpp
+++ SegDisp/display.cpp
@@ -31,6 +31,7 @@ void DisplayClass::asyncshow(char d1, char d2, char d3, bool dot1, bool dot2, bo
 	prev_time = time;
 
 	digitalWrite(10 + display, HIGH);
+	display = (display + 1) % 3;
 
 	unsigned char sym[3] = { 
 		(dot1 ? symbol[d1] | 1 : symbol[d1]), 
@@ -41,8 +42,6 @@ void DisplayClass::asyncshow(char d1, char d2, char d3, bool dot1, bool dot2, bo
 		digitalWrite(2 + seg, (sym[display] << seg & 128) / 128);
 
 	digitalWrite(10 + display, LOW);
-
-	display = (display + 1) % 3;
 }
 
 void DisplayClass::show(char d1, char d2, char d3, bool dot1, bool dot2, bool dot3) {
